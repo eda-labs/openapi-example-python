ec3c836598d4266601fa7bb94545958aa83bf126
Roman Dodin <dodin.roman@gmail.com>
2025-01-19 13:47:11 +0100

    basic logger

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from models.com.nokia.eda.interfaces.v1alpha1 import (
     Interface,
     Member,
@@ -6,11 +8,16 @@ from models.com.nokia.eda.interfaces.v1alpha1 import (
 )
 from src.client import Client
 
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 
 def main():
     c = Client(base_url="https://devbox.panda-cobra.ts.net")
     c.auth()
-    print(f"Access Token: {c.token}")
+    logger.info(f"Access Token: {c.token}")
 
     # iface = Interface(
     #     apiVersion="interfaces.eda.nokia.com/v1alpha1",
