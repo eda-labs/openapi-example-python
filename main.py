ffa83519d13692ac0150b6378ee5f4c182aca08b
hellt <dodin.roman@gmail.com>
2025-08-16 11:33:03 +0200

    update for services v1 and transaction v2

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ setup_logging()
 
 
 def main():
-    eda = EDAClient(base_url="https://<your_group_id>.srexperts.net:9443")
+    eda = EDAClient(
+        base_url="https://<your_group_id>.srexperts.net:9443",
+        username="admin",
+        password="<fill in the password>",
+    )
     my_virtualnetwork = virtualnetwork(ns="eda", name="my-vnet-using-python")
     eda.add_to_transaction_create(my_virtualnetwork)
     eda.commit_transaction()
